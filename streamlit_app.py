d262f852d21f276904d87ab46316361e64a90f9a
agungmagnetcuan-ai <agungmagnetcuan@gmail.com>
2026-05-24 21:07:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,14 @@ st.title("🎈 selamat datang kawan")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+import streamlit as st
+
+action = st.menu_button("Export", options=["CSV", "JSON", "PDF"])
+if action == "CSV":
+    st.write("Exporting as CSV...")
+elif action == "JSON":
+    st.write("Exporting as JSON...")
+elif action == "PDF":
+    st.write("Exporting as PDF...")
